af5cd6addac909507db4dfbed2f0f10149ce4034
Hans Huanca Aroni <hansaroni@hotmail.com>
2022-07-14 21:01:01 -0300

    servidor con express

---
+++ contenedor.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+
+class Contenedor{
+  constructor(file) {
+    this.file = file;
+  }
+
+  async save(prod){
+    try{
+      const data = await fs.promises.readFile(`./files/${this.file}.txt`, 'utf-8');
+      const products = (data.length === 0) ? [] : JSON.parse(data);
+      let id = products.length ? products[products.length-1].id+1 : 1;
+
+      products.push({...prod, id: id});
+      await fs.promises.writeFile(`./files/${this.file}.txt`, JSON.stringify(products));
+      console.log(`Producto agregado con éxito, el ID asignado es: ${id}`);
+    }
+    catch(err){
+      console.log("error", err);
+    }
+  }
+
+  async getById(id){
+    try{
+      const data = await fs.promises.readFile(`./files/${this.file}.txt`, 'utf-8');
+      console.log(JSON.parse(data).find(p => p.id === id));
+    }
+    catch(err){
+      console.log("error", err);
+    }
+  }
+
+  async getAll(){
+    try{
+      const data = await fs.promises.readFile(`./files/${this.file}.txt`, 'utf-8');
+      return JSON.parse(data);
+    }
+    catch(err){
+      console.log("error", err);
+    }
+  }
+
+  async deleteById(id){
+    try{
+      const data = await fs.promises.readFile(`./files/${this.file}.txt`, 'utf-8');
+      const products = JSON.parse(data);
+      await fs.promises.writeFile(`./files/${this.file}.txt`, JSON.stringify(products.filter(p => p.id !== id)));
+      console.log("Producto eliminado")
+    }
+    catch(err){
+      console.log("error", err);
+    }
+  } 
+
+  async deleteAll(){
+    try{
+      await fs.promises.writeFile(`./files/${this.file}.txt`, '');
+      console.log("Productos eliminados")
+    }
+    catch(err){
+      console.log("error", err);
+    }
+  }
+}
+
+module.exports = Contenedor;
---
+++ server.js
@@ -0,0 +1,36 @@
+const Contenedor = require('./contenedor.js');
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+const data = new Contenedor('Productos');
+
+app.get('/', async (req, res) =>{
+  res.send('Bienvenido! Prueba https://nodeserverjg.glitch.me/productos o https://nodeserverjg.glitch.me/productoRandom');
+})
+
+app.get('/productos', async (req, res) =>{
+  let prods;
+  try{
+    prods = await data.getAll();
+  } 
+  catch(err){
+    console.log("error", err);
+  }
+  res.send(prods);
+})
+
+app.get('/productoRandom', async (req, res) =>{
+  let prods;
+  try{
+    prods = await data.getAll();
+  } 
+  catch(err){
+    console.log("error", err);
+  }
+  res.send(prods[Math.floor(Math.random() * prods.length)]);
+})
+
+app.listen(PORT, ()=> {
+  console.log(`Servidor en puerto: ${PORT}`)
+});
