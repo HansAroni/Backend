40a42c6521511ba5dd762e14f350a4d1196b3cda
Hans Huanca Aroni <hansaroni@hotmail.com>
2022-07-31 22:04:56 -0300

    entrega-clase10

---
+++ entrega-clase10/handlebars/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const handlebars = require('express-handlebars')
+const { engine } = require( "express-handlebars");
+const PORT = 8080
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+//MOTOR
+
+
+app.engine("hbs", engine({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layout"
+
+
+}))
+const Contenedor = require('./utils/Contenedor');
+app.set('view engine','hbs');
+app.set('views', './views');
+
+
+const db = './utils/productos.json'
+
+const productos = new Contenedor(db)
+
+
+
+
+app.post('/productos', async (req, res) =>{
+    const item = req.body;
+    await productos.save(item);
+    res.redirect('/')
+})
+
+app.get('/productos', async (req, res) => {
+    const prods =  await productos.getAll()
+    
+
+    res.render("main", {
+        productos: prods,
+        hayProductos: prods.length
+    });
+})
+//LISTEN
+
+app.listen(PORT, () => {
+    console.log(`Puerto ${PORT} online`);
+
+})
---
+++ entrega-clase10/pug/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+const Contenedor = require('./utils/Contenedor');
+const db = './utils/productos.json'
+
+const productos = new Contenedor(db)
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+
+
+
+
+app.set('views', './view');
+app.set('view engine', 'pug')
+
+
+app.get('/productos', async (req, res) => {
+    const resultado = await productos.getAll()
+
+    res.render('vista', {
+        productos: resultado
+    })
+})
+app.post('/productos', async (req, res) =>{
+    const item = req.body;
+    await productos.save(item);
+    res.redirect('/')
+})
+
+// app.get('/productos', (req, res) => {
+//     const { nivel, min, max } = req.query
+//     res.render('desafio1', {
+//         titulo: '  MEDIDOR',
+//         nivel: nivel,
+//         min: min,
+//         max: max
+//     })
+// })
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor  escuchando en el puerto ${PORT}`)
+})
+server.on("error", error => console.log(`Error en servidor ${error}`))
