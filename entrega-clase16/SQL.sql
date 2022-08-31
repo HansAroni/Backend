bc9e05ffac7e3ea3d9dbd3f2d0ec8d1a6a4c9528
Hans Huanca Aroni <hansaroni@hotmail.com>
2022-08-30 21:57:38 -0300

    Entrega  clase 16 v2

---
+++ entrega-clase16/SQL.sql
@@ -0,0 +1,3 @@
+SHOW databases;
+CREATE DATABASE IF NOT EXISTS plataforma_db;
+USE plataforma_db;
---
+++ entrega-clase16v2/sql.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS plataforma_db;
+USE plataforma_db;
+SHOW databases;
+show tables;
+
